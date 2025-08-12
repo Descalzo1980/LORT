513de7c3de5683d636241600a9bbce55adee3a25
Stas <ragioniere1980@gmail.com>
2025-08-12 11:54:01 +0300

    add exchange screen
    add exchange screen

--- LOTRConverter/ExchangeInfo.swift
+++ LOTRConverter/ExchangeInfo.swift
@@ -17,19 +17,10 @@ struct ExchangeInfo: View {
                     .font(.title3)
                     .padding()
                 
-                HStack {
-                    Image(.goldpiece)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                    
-                    Text("1 Gold Piece = 4 Gold Pennies")
-                    
-                    Image(.goldpenny)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 33)
-                }
+                ExchangeRate(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+                ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage:.silverpiece)
+                ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage:.silverpenny)
+                ExchangeRate(leftImage: .silverpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage:.copperpenny)
                 Button("Done") {
                     
                 }
---
+++ LOTRConverter/ExchangeRate.swift
@@ -0,0 +1,35 @@
+//
+//  ExchangeRate.swift
+//  LOTRConverter
+//
+//  Created by Станислав Леонов on 12.08.2025.
+//
+
+import SwiftUI
+
+struct ExchangeRate: View {
+    let leftImage: ImageResource
+    let text: String
+    let rightImage: ImageResource
+
+    
+    var body: some View {
+        HStack {
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            
+            Text(text)
+            
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+        }
+    }
+}
+
+#Preview {
+    ExchangeRate(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+}
