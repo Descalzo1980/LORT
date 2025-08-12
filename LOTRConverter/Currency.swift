28f980c5157485b4254268fff9930c20454e11d7
Stas <ragioniere1980@gmail.com>
2025-08-12 19:46:27 +0300

    add model
    add model

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,16 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Станислав Леонов on 12.08.2025.
+//
+
+import Foundation
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
